9f51b03efeb487c12d6277bca5d3aef9d8bc4b42
Savaş Enes ERATEŞLİ <76652961+savaseeratesli@users.noreply.github.com>
2025-11-07 00:32:26 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Yeni tag verildi mi?");
+app.MapGet("/", () => "Yeni tag verildi mi? Evet başarılı.");
 
 app.Run("http://0.0.0.0:9080");
